9622baadab9786374831bcbd4507f2c3a0478452
Ariadne Engelbrecht <ariadne.engelbrecht@thenativeweb.io>
2022-08-02 21:45:28 +0200

    Add footer-line as web component

---
+++ components/cdc-footer.js
@@ -0,0 +1,24 @@
+//Built web component for footer:
+class Footer extends HTMLElement {
+  constructor() {
+    super();
+    let shadow = this.attachShadow({ mode: 'open' });
+    shadow.innerHTML = `
+      <style>
+        :host { 
+          @import url('https://fonts.googleapis.com/css2?family=Beth+Ellen&display=swap');
+          --font-handwritten-sloppy: 'Beth Ellen', cursive;
+        }
+
+        section {
+          font-family: var(--font-handwritten-sloppy);
+          font-size: 3rem;
+      </style>
+
+        <section>
+        <p>✨ Built in 🇪🇺🇩🇪…  with copious amounts of 🫀 && 🧠 && ☕️ && 🎉 by <a href='https://twitter.com/curiosdevcookie'> @curiosdevcookie</a> .✨</p>
+        </section>
+    `;
+  }
+}
+customElements.define('cdc-footer', Footer);
